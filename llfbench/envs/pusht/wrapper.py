0fa5fac052462082c1a6e450ded9e4bb1e5cd4ce
Skylar <hoyunjie@gmail.com>
2025-05-14 18:48:29 +0800

    fix(push-T wrapper logic): improve the feedback logic

--- llfbench/envs/pusht/wrapper.py
+++ llfbench/envs/pusht/wrapper.py
@@ -4,10 +4,10 @@ import numpy as np
 from llfbench.envs.llf_env import LLFWrapper, Feedback
 from llfbench.envs.pusht.prompts import *
 from llfbench.envs.pusht import task_feedback
-from llfbench.envs.pusht.utils_prompts.degree_prompts import degree_adverb_converter
-from llfbench.envs.pusht.utils_prompts.direction_prompts import direction_converter
+from llfbench.envs.pusht.utils_prompts.degree_prompts import move_degree_adverb_converter, turn_degree_adverb_converter
+from llfbench.envs.pusht.utils_prompts.direction_prompts import move_direction_converter, turn_direction_converter
 from llfbench.envs.pusht.utils_prompts.conjunction_prompts import positive_conjunctions_sampler, negative_conjunctions_sampler
-from llfbench.envs.pusht.utils_prompts.recommend_prompts import recommend_templates_sampler
+from llfbench.envs.pusht.utils_prompts.recommend_prompts import move_recommend_templates_sampler, turn_recommend_templates_sampler
 import importlib
 import json
 from textwrap import dedent, indent
@@ -27,7 +27,7 @@ class PushTWrapper(LLFWrapper):
     def __init__(self, env, instruction_type, feedback_type, debug: bool = False):
         super().__init__(env, instruction_type, feedback_type)
         # load the scripted policy
-        self._policy_name = f"solve{self.env.env_name}"[:-3] # remove version postfix
+        self._policy_name = "solvePushTKeypoints" if self.env.env_name=='llf-pusht-keypoints-v0' else "solvePushTImage" # remove version postfix
 
         if 'fp' in feedback_type:
             self._policy = getattr(solve_policy, self._policy_name)(
@@ -40,13 +40,15 @@ class PushTWrapper(LLFWrapper):
         self.control_relative_position = False
         self._current_observation = None
         self._prev_expert_action = None
-        self._curr_block_to_goal_distance = 1e8
-        self._curr_block_to_goal_angle = np.pi
+        self._curr_agent_to_block_distance = None
+        self._curr_block_to_goal_distance = None
+        self._curr_block_to_goal_angle = None
+        self.is_moved_to_goal = False
+        self.is_aligned_with_goal = False
         self.mode = 'rgb_array'
 
         if self.env.env_name == 'llf-pusht-keypoints-v0':
             self._step = self._step_keypoints
-            print("Using keypoints-based observation")
         else:
             self._step = self._step_image
 
@@ -68,22 +70,24 @@ class PushTWrapper(LLFWrapper):
         return self._current_observation
     
     @property
-    def _current_observation_keypoints(self):
+    def current_observation_keypoints(self):
         """ This is a cache of the latest (raw) observation. """
         return self._current_observation[:18]
     
     @property
-    def _current_agent_position(self):
+    def current_agent_position(self):
         """ This is a cache of the latest (raw) observation. """
         return self._current_observation[18:20]
 
     # auxiliary functions for language feedback
-    @property
     def expert_action(self):
         # Flatten current observation first
         obs_state = spaces.flatten(self.env.observation_space, self._current_observation)
-        expert_action = self.pt_policy.get_action(obs_state).squeeze(0).cpu().detach().numpy()
-        return expert_action
+        expert_action = self.pt_policy.get_action(obs_state)
+        return expert_action['action'][0].cpu().numpy()
+    
+    def compute_angle_diff(self, goal_angle, block_angle):
+        return (goal_angle - block_angle + np.pi) % (2 * np.pi) - np.pi
     
     # step functions for keypoints-based observation
     def _step_keypoints(self, action):
@@ -93,15 +97,30 @@ class PushTWrapper(LLFWrapper):
         feedback_type = self._feedback_type
         
         # Environment Features
-        prev_agent_posi = self._current_agent_position
-        prev_block_kps = self._current_observation_keypoints
+        # previous info
+        prev_agent_posi = self.current_agent_position
+        prev_block_kps = self.current_observation_keypoints
         prev_block_to_goal_distance = self._curr_block_to_goal_distance
         prev_block_to_goal_angle = self._curr_block_to_goal_angle
+        prev_agent_to_block_distance = self._curr_agent_to_block_distance
+        prev_is_moved_to_goal = self.is_moved_to_goal
+        # prev_is_moved_away_from_goal = self.is_moved_away_from_goal
+        prev_is_aligned_with_goal = self.is_aligned_with_goal
+        # prev_is_misaligned_with_goal = self.is_misaligned_with_goal
+        # prev_agent_move_to_the_block = self.agent_move_to_the_block
+
+        # current info
         self._current_observation = observation
-        self._curr_block_to_goal_distance = np.linalg.norm(info['block_pose'][:-1] - info['goal_pose'][:-1])
-        self._curr_block_to_goal_angle = min(info['goal_pose'][-1] - info['block_pose'][-1], 2 * np.pi - abs(info['goal_pose'][-1] - info['block_pose'][-1]))
-        is_moved_to_goal = self._curr_block_to_goal_distance < prev_block_to_goal_distance
-        is_aligned_with_goal = self._curr_block_to_goal_angle < prev_block_to_goal_angle
+        self._curr_block_to_goal_distance = np.linalg.norm(info['goal_pose'][:-1] - info['block_pose'][:-1])
+        self._curr_block_to_goal_angle = self.compute_angle_diff(info['goal_pose'][-1], info['block_pose'][-1])
+        self._curr_agent_to_block_distance = np.linalg.norm(info['block_pose'][:-1] - info['pos_agent'])
+        self.is_moved_to_goal = False if prev_block_to_goal_distance == None else self._curr_block_to_goal_distance < prev_block_to_goal_distance
+        self.is_moved_away_from_goal = False if prev_block_to_goal_distance == None else self._curr_block_to_goal_distance > prev_block_to_goal_distance
+        self.is_aligned_with_goal =  False if prev_block_to_goal_angle == None else self._curr_block_to_goal_angle < prev_block_to_goal_angle
+        self.is_misaligned_with_goal =  False if prev_block_to_goal_angle == None else self._curr_block_to_goal_angle > prev_block_to_goal_angle
+        self.agent_move_to_the_block = True if prev_block_to_goal_angle == None else self._curr_agent_to_block_distance < prev_agent_to_block_distance
+        self._goal_pose = info['goal_pose']
+        self._curr_block_pose = info['block_pose']
 
         # Prerequisites for staging, action, and recommendation feedbacks.
         """
@@ -115,53 +134,63 @@ class PushTWrapper(LLFWrapper):
         2. Rotate the block towards the goal
         """
         
-        if reward == 0:
+        # stage feedback
+        if info['n_contacts'] == 0:
+            _stage_feedback = self.format(task_feedback.move_to_proper_contact_posi_feedback)
+        elif reward == 0:
             _stage_feedback = self.format(task_feedback.move_T_shaped_block_to_the_goal_feedback)
         else:
             _stage_feedback = self.format(task_feedback.align_T_shaped_block_and_the_goal_region_feedback)
 
+        # recommend feedback
+        move_direction = move_direction_converter(self._goal_pose[:-1] - self._curr_block_pose[:-1])
+        move_degree = move_degree_adverb_converter(self._goal_pose[:-1] - self._curr_block_pose[:-1])
 
-        if is_moved_to_goal or is_aligned_with_goal:
-            moving_axis = [cbhpp > pbhpp for cbhpp, pbhpp in zip(abs(self._current_agent_position), abs(prev_agent_posi))]
-            direction = direction_converter(self._current_agent_position - prev_agent_posi)
-            degree = degree_adverb_converter(self._current_agent_position - prev_agent_posi)
-            _recommend_feedback = [
-                recommend_templates_sampler().format(degree=degree, direction=direction)
-                for away, direction, degree in zip(moving_axis, direction, degree) if away
-            ]
-        else:
-            moving_axis = [cbhpp < pbhpp for cbhpp, pbhpp in zip(torch.abs(self._current_agent_position), torch.abs(prev_agent_posi))]
-            direction = direction_converter(prev_agent_posi - self._current_agent_position)
-            degree = degree_adverb_converter(prev_agent_posi - self._current_agent_position)
-            _recommend_feedback = [
-                recommend_templates_sampler().format(degree=degree, direction=direction)
-                for away, direction, degree in zip(moving_axis, direction, degree) if away
-            ]
+        turn_direction = turn_direction_converter(self._curr_block_to_goal_angle)
+        turn_degree = turn_degree_adverb_converter(self._curr_block_to_goal_angle)
 
-        if is_aligned_with_goal and is_moved_to_goal:
+        _recommend_feedback = [
+            move_recommend_templates_sampler().format(degree=degree, direction=direction)
+            for direction, degree in zip(move_direction, move_degree)
+        ] + [
+            turn_recommend_templates_sampler().format(direction=turn_direction, degree=turn_degree)
+        ]
+
+        # reason feedback
+        positive_hindsight = True
+        if self.is_aligned_with_goal and self.is_moved_to_goal:
             _reason_feedback_move = self.format(task_feedback.move_T_shaped_block_to_the_goal_reason)
             _reason_feedback_align = self.format(task_feedback.align_T_shaped_block_and_the_goal_region_reason)
             _reason_feedback = _reason_feedback_move[:-1] + ' and ' + _reason_feedback_align[17:]
-        elif is_moved_to_goal:
+        elif self.is_moved_to_goal:
             _reason_feedback = self.format(task_feedback.move_T_shaped_block_to_the_goal_reason)
-        elif is_aligned_with_goal:
+        elif self.is_aligned_with_goal:
             _reason_feedback = self.format(task_feedback.align_T_shaped_block_and_the_goal_region_reason)
-        elif not is_moved_to_goal:
+        elif self.is_moved_away_from_goal:
+            positive_hindsight = False
             _reason_feedback = self.format(task_feedback.move_T_shaped_block_away_from_the_goal_reason)
-        else:
+        elif self.is_misaligned_with_goal:
+            positive_hindsight = False
             _reason_feedback = self.format(task_feedback.misaligned_T_shaped_block_reason)
+        else:
+            # determine hp or hn
+            # if the previous action is good and the eef leaves, it is bad to navigate to the new contact point
+            if (prev_is_moved_to_goal or prev_is_aligned_with_goal):
+                positive_hindsight = False
+                _reason_feedback = self.format(task_feedback.leave_the_proper_contact_posi_reason)
+            else:
+                _reason_feedback = self.format(task_feedback.move_to_proper_contact_posi_reason)
 
         # Set actions
-        # TODO: use the expert policy to get the action
         if 'fp' in feedback_type:
-            expert_action = self.expert_action
+            expert_action = self.expert_action()
             self._prev_expert_action = expert_action.copy()
         
         feedback = Feedback()
         if 'r' in feedback_type:
             feedback.r = self.format(r_feedback, reward=np.round(reward, 3))
 
-        if 'hp' in feedback_type:
+        if 'hp' in feedback_type and positive_hindsight:
             _feedback = self.concatenate_sentences(
                 stage_feedback = _stage_feedback,
                 action_feedback = self.format(hp_feedback, reason=_reason_feedback),
@@ -170,7 +199,7 @@ class PushTWrapper(LLFWrapper):
             )
             feedback.hp = _feedback
 
-        if 'hn' in feedback_type:
+        if 'hn' in feedback_type and not positive_hindsight:
             _feedback = self.concatenate_sentences(
                 stage_feedback = _stage_feedback,
                 action_feedback = self.format(hn_feedback, reason=_reason_feedback),
@@ -183,7 +212,7 @@ class PushTWrapper(LLFWrapper):
             feedback.fp = self.format(fp_feedback, expert_action=self.textualize_expert_action(expert_action))
 
         observation = self._format_obs()
-        return dict(instruction=None, observation=observation, feedback=feedback), float(reward), terminated.cpu().item(), truncated.cpu().item(), info
+        return dict(instruction=None, observation=observation, feedback=feedback), float(reward), terminated, truncated, info
 
     # step functions for image-based observation
     def _step_image(self, action):
@@ -204,7 +233,7 @@ class PushTWrapper(LLFWrapper):
         return dict(instruction=instruction, observation=observation, feedback=None), info
 
     def _format_obs(self):
-        obs_dict = {'keypoints': self._current_observation_keypoints, 'agent_posi': self._current_agent_position}
+        obs_dict = {'keypoints': self.current_observation_keypoints, 'agent_posi': self.current_agent_position}
         text = self.textualize_observation(obs_dict)
         image = (self.env.render(mode=self.mode) if self.env.visual else None)
         return text if image is None else dict(text=text, image=image)
