c5a070573aa2c5d2937d6d98848606be31a1c5d4
Skylar <hoyunjie@gmail.com>
2025-05-14 18:45:38 +0800

    feat(push-T turning direction prompts): add turn_direction_converter and direction list and dictionary fix(push-T moving direction prompts): return empty string when difference is zero

--- llfbench/envs/pusht/utils_prompts/direction_prompts.py
+++ llfbench/envs/pusht/utils_prompts/direction_prompts.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 
-direction_desc_dict = {
+move_direction_desc_dict = {
     "x-axis": {
         "pos": ["rightward", "to the right", "toward the right"],
         "neg": ["leftward", "to the left", "toward the left"]
@@ -12,7 +12,7 @@ direction_desc_dict = {
     },
 }
 
-direction_desc_list = [
+move_direction_desc_list = [
     [
         ["leftward", "to the left", "toward the left"],
         ["rightward", "to the right", "toward the right"] 
@@ -23,9 +23,28 @@ direction_desc_list = [
     ],
 ]
 
-def direction_converter(difference: np.array):
-    # TODO: need to check the difference
-    return [
-        random.choice(direction_desc_list[i][int(difference[i] < 0)])
-        for i in range(2)
+turn_direction_desc_dict = {
+    "angle": {
+        "pos": ["clockwise", "rightward around the center", "with positive angle"],
+        "neg":  ["counterclockwise", "leftward around the center", "with negative angle"],
+    }
+}
+
+turn_direction_desc_list = [
+    [
+        ["counterclockwise", "against the direction of a clock"],
+        ["clockwise", "in the direction of a clock"],
     ]
+]
+
+def move_direction_converter(difference: np.array):
+    return [
+        random.choice(move_direction_desc_list[i][int(difference[i] > 0)])
+        if difference[i] != 0 else ""
+        for i in range(difference.shape[0])
+    ]
+
+def turn_direction_converter(difference: float):
+    if difference == 0:
+        return ""
+    return random.choice(turn_direction_desc_list[0][int(difference > 0)])
