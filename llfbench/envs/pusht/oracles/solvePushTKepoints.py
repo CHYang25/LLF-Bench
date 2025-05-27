259a9fd3f8ed3b27cdfa6ef8949cb20fe2d7a7b5
Skylar <hoyunjie@gmail.com>
2025-05-27 11:35:51 +0800

    feat: add new checkpoints

--- llfbench/envs/pusht/oracles/solvePushTKepoints.py
+++ llfbench/envs/pusht/oracles/solvePushTKepoints.py
@@ -1,5 +1,6 @@
 from llfbench.envs.pusht.pusht.pusht_keypoints_env import PushTKeypointsEnv
 from llfbench.envs.pusht.oracles.solvePushT import solvePushT
+import os
 
 class solvePushTKeypoints(solvePushT):
 
@@ -8,7 +9,9 @@ class solvePushTKeypoints(solvePushT):
 
         super().__init__(
             env, 
-            checkpoint_dir="LLF-Bench/llfbench/envs/pusht/oracles/pusht_keypoints_checkpoints/epoch=3000-test_mean_score=0.903.ckpt",
+            # checkpoint_dir=os.getcwd()+"/LLF-Bench/llfbench/envs/pusht/oracles/pusht_keypoints_checkpoints/epoch=1100-test_mean_score=0.878.ckpt",
+            checkpoint_dir=os.getcwd()+"/LLF-Bench/llfbench/envs/pusht/oracles/pusht_keypoints_checkpoints/epoch=0100-test_mean_score=0.578.ckpt",
+            # checkpoint_dir=os.getcwd()+"/LLF-Bench/llfbench/envs/pusht/oracles/pusht_keypoints_checkpoints/epoch=0050-test_mean_score=0.143.ckpt",
             device=device,
             seed=seed,
             debug=debug,
