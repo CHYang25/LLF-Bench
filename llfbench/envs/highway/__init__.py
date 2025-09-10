e8ca1a4834ef49df80b16b79c6b99523a1a9f7ae
Skylar <hoyunjie@gmail.com>
2025-09-10 15:13:36 +0800

    feat(parking-v0): add new wrapper for original pakring env

--- llfbench/envs/highway/__init__.py
+++ llfbench/envs/highway/__init__.py
@@ -1,7 +1,12 @@
+import random
+import warnings
 import gymnasium as gym
 from gymnasium.envs.registration import register
+import numpy as np
 from llfbench.utils import generate_combinations_dict
 from llfbench.envs.highway.wrapper import HighwayWrapper
+from gymnasium.wrappers import TimeLimit
+import sys
 
 ENVIRONMENTS = (
     'parking-v0',
@@ -11,11 +16,53 @@ ENVIRONMENTS = (
 def make_env(env_name,
              instruction_type='b',
              feedback_type='a',
-             visual=False):
-    assert visual == False, "The highway environment has no visual observations"
-    """ Make the original env and wrap it with the LLFWrapper. """
-    env = gym.make(env_name)
-    return HighwayWrapper(env, instruction_type=instruction_type, feedback_type=feedback_type)
+             visual=False,
+             seed=0,
+             warning=True):
+    # assert visual == False, "The highway environment has no visual observations"
+    sys.path.insert(0, '/content/HighwayEnv/scripts/')
+
+    if visual:
+        from gymnasium.wrappers import RecordVideo
+        env = gym.make(env_name, render_mode='rgb_array')
+        env = RecordVideo(
+            env, video_folder='./media/', episode_trigger=lambda e: False
+        )
+    else:
+        env = gym.make(env_name)
+
+    class Wrapper(gym.Wrapper):
+         # a small wrapper to make sure the task is set
+         # and to make the env compatible with the old gym api
+        def __init__(self, env):
+            super().__init__(env)
+            self.env.max_path_length = float('inf')
+            # We remove the internal time limit. We will redefine the time limit in the wrapper.
+            self._render_video = False
+            self.visual = visual
+
+        def render_video(self, value):
+            self._render_video = value
+            if value:
+                self.env.render_mode = 'rgb_array'
+
+        @property
+        def env_name(self):
+            return env_name
+        
+        def reset(self, *, seed=None, options=None):
+            if seed is not None:
+                random.seed(seed)
+                np.random.seed(seed)
+            return self.env.reset(seed=seed, options=options)
+        
+    env = Wrapper(env)
+
+    if not warning:
+        gym.logger.set_level(gym.logger.ERROR)
+        warnings.filterwarnings("ignore")
+    
+    return TimeLimit(HighwayWrapper(env, instruction_type=instruction_type, feedback_type=feedback_type), max_episode_steps=1000)
 
 for env_name in ENVIRONMENTS:
     # default version (backwards compatibility)
