f6c5617f6df2fd7bc02ca263fb9272ddcce19225
CHYang25 <chris920325@gmail.com>
2025-09-26 14:37:56 +0800

    update(blockpushing_install.sh): remove uncessary dependency

--- blockpushing_install.sh
+++ blockpushing_install.sh
@@ -2,8 +2,7 @@ pip install pybullet==3.2.6
 pip install tf-agents==0.19.0
 pip install typing-extensions==4.12.2
 pip install pygame==2.6.1
-pip install gym==0.26.2
 pip install tensorflow==2.15.1
 pip install mujoco==2.3.7
-pip insatll gym==0.23.1
-pip install PyOpenGL==3.1.1a1
+pip install gym==0.23.1
+pip install PyOpenGL==3.1.10
