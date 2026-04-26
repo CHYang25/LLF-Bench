8d87c352e4aaa26a44a526f882383803a6279589
CHYang25 <chris920325@gmail.com>
2026-04-26 15:33:09 +0800

    update(adroit/__init__.py): enhance randomness for adroit-hand-hammer

--- llfbench/envs/adroit/__init__.py
+++ llfbench/envs/adroit/__init__.py
@@ -10,8 +10,15 @@ import time
 from gymnasium.wrappers import TimeLimit
 import numpy as np
 import os
+import types
 
 gym.register_envs(gymnasium_robotics)
+
+def custom_reset_model_adroit_hand_hammer_v1(self):
+    target_bid = self._model_names.body_name2id["nail_board"]
+    self.model.body_pos[target_bid, 2] = self.np_random.uniform(low=0.1, high=0.3)
+    self.set_state(self.init_qpos, self.init_qvel)
+    return self._get_obs()
 
 def make_env(env_name,
              instruction_type='b',
@@ -29,6 +36,7 @@ def make_env(env_name,
             super().__init__(env)            
             self._render_video = False
             self.visual = visual
+            self.enhance_random = False
 
         def render_video(self, value):
             self._render_video = value
@@ -43,6 +51,19 @@ def make_env(env_name,
             if seed is not None:
                 random.seed(seed)
                 np.random.seed(seed)
+
+            if options is not None:
+                if options.get("enhance_random", False):
+                    if not self.enhance_random:
+                        if self.env_name == "AdroitHandHammer-v1":
+                            base_env = self.env.unwrapped
+                            base_env.reset_model = types.MethodType(custom_reset_model_adroit_hand_hammer_v1, base_env)
+                            self.enhance_random = True
+                else:
+                    base_env = self.env.unwrapped
+                    base_env.reset_model = types.MethodType(gymnasium_robotics.envs.adroit_hand.adroit_hammer.AdroitHandHammerEnv.reset_model, base_env)
+                    self.enhance_random = False
+
             return self.env.reset(seed=seed, options=options)
         
     env = Wrapper(env)
