0937dc13228bf5deb20599a52addd1c299c4762b
CHYang25 <chris920325@gmail.com>
2026-07-16 15:33:31 +0800

    update(conjunction_prompts): metaworld

--- llfbench/envs/metaworld/utils_prompts/conjunction_prompts.py
+++ llfbench/envs/metaworld/utils_prompts/conjunction_prompts.py
@@ -4,9 +4,14 @@ positive_conjunctions = [
     " Also, ", " Moreover, ", " Furthermore, ", " Plus, ", 
 ]
 
+progressive_conjunctions = [
+    " Then, ", " Next, ", " Afterward, ", " After that, ",
+]
+
 negative_conjunctions = [
     " However, ", " But, ", " Yet, ", " Even so, ",
 ]
 
 positive_conjunctions_sampler = lambda: random.choice(positive_conjunctions)
+progressive_conjunction_sampler = lambda: random.choice(progressive_conjunctions)
 negative_conjunctions_sampler = lambda: random.choice(negative_conjunctions)
